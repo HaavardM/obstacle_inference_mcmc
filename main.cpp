a0ee5c886f5508f8491c7ad55ec29fa8cb033f44
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2020-10-07 21:14:21 +0200

    Fix intention ordering

--- main.cpp
+++ main.cpp
@@ -36,25 +36,25 @@ std::tuple<arma::vec, arma::Col<int>> load_csv() {
 
 auto data = load_csv();
 
-arma::mat softmax(arma::mat p) {
+arma::mat softmax(const arma::mat& p) {
     auto exp = arma::exp(p);
     auto sum = arma::sum(exp, 0);
     return exp * 1.0 / arma::repelem(sum, 3, 1);
 }
 
-arma::vec3 map_probs(double theta, double c, arma::vec3 alpha) {
+arma::vec3 map_probs(double theta, double c, const arma::vec3& alpha) {
     const arma::mat33 m =
-        theta >= 0.0
+        theta <= 0.0
             ? arma::mat33({{(std::exp(-c * std::abs(theta))), 1.0 / 2.0, 0.0},
-                           {(1 - std::exp(-c * theta)), 1.0 / 2.0, 0.0},
+                           {(1 - std::exp(c * theta)), 1.0 / 2.0, 0.0},
                            {(0.0), 0.0, 1.0}})
             : arma::mat33({{(std::exp(-c * std::abs(theta))), 1.0 / 2.0, 0.0},
                            {(0.0), 0.0, 1.0},
-                           {(1 - std::exp(c * theta)), 1.0 / 2.0, 0.0}});
+                           {(1 - std::exp(-c * theta)), 1.0 / 2.0, 0.0}});
     return m * alpha;
 }
 
-double ll_dirchilet(arma::vec3 x, arma::vec3 p) {
+double ll_dirchilet(const arma::vec3& x, const arma::vec3& p) {
     return arma::sum(
                arma::vec3((p - arma::vec3(arma::fill::ones)) % arma::log(x) -
                           arma::vec3({gcem::lgamma(p[0]), gcem::lgamma(p[1]),
@@ -69,31 +69,31 @@ double ll_gamma(double x, double shape, double scale) {
 
 double log_likelihood(const arma::vec &p, void *ll_data) {
     auto c = std::exp(p[0]);
-    auto a = softmax(arma::vec3({p[1], p[2], p[3]}));
+    auto a = softmax(p.rows(1,3));
 
     auto &[theta, intention] = data;
 
-    auto probs = arma::mat(3, theta.n_rows);
+    static auto probs = arma::mat(3, theta.n_rows);
     #pragma omp parallel for
     for (int i = 0; i < theta.n_rows; ++i) {
         probs.col(i) = map_probs(theta[i], c, a);
     }
     arma::mat lprobs = arma::log(probs);
     auto ll_c = ll_gamma(c, 2.0, 2.0);
-    auto ll_a = ll_dirchilet(a, {4.0, 2.0, 1.0});
+    auto ll_a = ll_dirchilet(a, {1.0, 1.0, 1.0});
     auto ll_intention = 0.0;
     #pragma omp parallel for reduction(+:ll_intention)
     for (int i = 0; i < intention.n_rows; ++i) {
         ll_intention += lprobs.col(i)[intention[i]];
     }
-    auto ll = ll_c + ll_a + ll_intention / intention.n_rows;
+    auto ll = ll_c + ll_a + ll_intention;// / intention.n_rows;
     static int i = 0;
     if (i++ % 10000 == 0) {
         std::cout << i << ": " << ll << std::endl;
-        printf("* c: %.4f - %.4f\n", c, ll_c);
-        printf("* a1: %.4f - %.4f\n", a[0], ll_a);
-        printf("* a2: %.4f - %.4f\n", a[1], ll_a);
-        printf("* a3: %.4f - %.4f\n", a[2], ll_a);
+        printf("* c:     %.4f - %.4f\n", c, ll_c);
+        printf("* a1:    %.4f - %.4f\n", a[0], ll_a);
+        printf("* a2:    %.4f - %.4f\n", a[1], ll_a);
+        printf("* a3:    %.4f - %.4f\n", a[2], ll_a);
         printf("* ll_intention: %.4f\n", ll_intention);
     }
     return ll;
