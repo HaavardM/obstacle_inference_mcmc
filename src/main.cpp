810738aba9b0d425d062b1ba47dd9812672efbc6
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2020-10-10 15:26:05 +0200

    Fix Eigen

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,22 @@
 #include "mcmc.hpp"
 
-#define STATS_ENABLE_ARMA_WRAPPERS
-
 #include <cmath>
 #include <iostream>
 
+#include <armadillo>
 #include "csv.hpp"
 #include "gcem.hpp"
-#include "stats.hpp"
 #include <omp.h>
 #include "matplotlibcpp.h"
 
+#define EIGEN_USE_BLAS
 #include "Eigen/Dense"
+#include "unsupported/Eigen/SpecialFunctions"
 #include <algorithm>
 
-#include <armadillo>
 
 constexpr auto P = 4;
-constexpr auto N = 100;
+constexpr auto N = 1000;
 
 constexpr double PI = 3.14;
 
@@ -33,7 +32,7 @@ std::tuple<Eigen::Vector<double, N>, Eigen::Vector<int, N>> load_csv() {
     for (csv::CSVRow &row : reader) {
         theta_vec.push_back(row[0].get<double>());
         intention_vec.push_back(row[1].get<int>());
-        if (++i >= 100) {
+        if (++i >= N) {
             break;
         }
     }
@@ -50,26 +49,26 @@ std::tuple<Eigen::Vector<double, N>, Eigen::Vector<int, N>> load_csv() {
 auto data = load_csv();
 
 template<typename D>
-void softmax(Eigen::MatrixBase<D>& p) {
+void apply_softmax(Eigen::MatrixBase<D>& p) {
     auto exp = p.array().exp();
     p = exp.array().rowwise() / exp.array().colwise().sum();
 }
 
-Eigen::Vector3d map_probs(double theta, double c, const Eigen::Vector3d& alpha) {
-    const Eigen::Matrix3d m =
-        theta <= 0.0
-            ? Eigen::Matrix3d({{(std::exp(-c * std::abs(theta))), 1.0 / 2.0, 0.0},
+double intention_probs(double theta, double c, const Eigen::Vector3d& alpha, int intention) {
+    if (theta <= 0.0) {
+        return (Eigen::Matrix<double, 3, 3, Eigen::RowMajor>({{(std::exp(-c * std::abs(theta))), 1.0 / 2.0, 0.0},
                            {(1 - std::exp(c * theta)), 1.0 / 2.0, 0.0},
-                           {(0.0), 0.0, 1.0}})
-            : Eigen::Matrix3d({{(std::exp(-c * std::abs(theta))), 1.0 / 2.0, 0.0},
+                           {(0.0), 0.0, 1.0}}).row(intention) * alpha);
+    } else {
+        return (Eigen::Matrix<double, 3, 3, Eigen::RowMajor>({{(std::exp(-c * std::abs(theta))), 1.0 / 2.0, 0.0},
                            {(0.0), 0.0, 1.0},
-                           {(1 - std::exp(-c * theta)), 1.0 / 2.0, 0.0}});
-    return m * alpha;
+                           {(1 - std::exp(-c * theta)), 1.0 / 2.0, 0.0}}).row(intention) * alpha);
+    }
 }
 
 double ldirchilet(const Eigen::Vector3d& x, const Eigen::Vector3d& p) {
-    return ((p.array() - 1.0) * x.array().log() - Eigen::Vector3d({std::lgamma(p[0]), gcem::lgamma(p[1]), gcem::lgamma(p[2])}).array()).sum() +
-           gcem::lgamma(p.sum());
+    auto v = (p.array() - 1.0) * x.array().log() - p.array().lgamma();
+    return v.sum() + gcem::lgamma(p.sum());
 }
 
 double lgamma(double x, double shape, double scale) {
@@ -77,37 +76,47 @@ double lgamma(double x, double shape, double scale) {
            shape * std::log(scale);
 }
 
-double log_likelihood(const arma::vec &arma_p, void *ll_data) {
+double log_prior(const Eigen::Vector4<double> p) {
+    double lp_c = lgamma(p(0)*10, 7.5, 1.0);
+    double lp_a = ldirchilet(p.tail(3), Eigen::Vector3d({20, 4, 2.5}));
+    return lp_c + lp_a;
+}
 
-    const auto p = Eigen::Map<const Eigen::VectorXd>(arma_p.memptr(), arma_p.n_rows);
-    auto c = std::exp(p(0) * RW_SCALING);
-    Eigen::Vector3d a = p.tail(3) * RW_SCALING; 
-    softmax(a);
-
+double log_likelihood(const Eigen::Vector4<double> p) {
     auto &[theta, intention] = data;
 
-    Eigen::Matrix<double, 3, N> probs;
-    #pragma omp parallel for
-    for (int i = 0; i < N; ++i) {
-        probs.col(i) = map_probs(theta(i), c, a);
-    }
-    Eigen::Matrix<double, 3, N> lprobs = probs.array().log();
-    auto lp_c = lgamma(c*10, 7.5, 1.0);
-    auto lp_a = ldirchilet(a, {20, 4, 2.5});
     auto ll_intention = 0.0;
     #pragma omp parallel for simd reduction(+:ll_intention)
     for (int i = 0; i < intention.size(); ++i) {
-        ll_intention += lprobs(intention(i), i);
+        auto c = p(0);
+        auto a = p.tail<3>();
+        ll_intention += std::log(intention_probs(theta(i), p(0), a, intention(i)));
     }
+
+    return ll_intention;
+}
+
+double log_target(const arma::vec &arma_p, void *ll_data) {
+    Eigen::Vector4d p = {
+        arma_p[0],
+        arma_p[1],
+        arma_p[2],
+        arma_p[3]
+    };
+    p *= RW_SCALING;
+    p(0) = std::exp(p(0));
+    auto a = p.bottomRows<3>();
+    apply_softmax(a);
+
     static int i = 0;
-    auto ll = lp_c + lp_a;// + ll_intention;// / static_cast<double>(intention.size());// / intention.n_rows;
+    auto ll = log_prior(p) + log_likelihood(p);// / static_cast<double>(intention.size());// / intention.n_rows;
     if (++i % 10000 == 0) {
         std::cout << i << ": " << ll << std::endl;
-        printf("* c:     %.4f - %.4f\n", c, lp_c);
-        printf("* a1:    %.4f - %.4f\n", a(0), lp_a);
-        printf("* a2:    %.4f - %.4f\n", a(1), lp_a);
-        printf("* a3:    %.4f - %.4f\n", a(2), lp_a);
-        printf("* ll_intention: %.4f\n", ll_intention);
+        printf("* c:     %.4f\n", p(0));
+        printf("* a1:    %.4f\n", p(1));
+        printf("* a2:    %.4f\n", p(2));
+        printf("* a3:    %.4f\n", p(3));
+        printf("* ll_intention: %.4f\n", log_likelihood(p));
     }
     return ll;
 }
@@ -117,13 +126,15 @@ std::tuple<Eigen::VectorXd, Eigen::Matrix3Xd> run(mcmc::algo_settings_t settings
     initial.row(0) *= 2;
     arma::mat arma_draws;
     std::cout << "Starting MCMC - Chain Number " << omp_get_thread_num() << std::endl;
-    mcmc::rwmh(initial, arma_draws, log_likelihood, nullptr, settings);
+    mcmc::rwmh(initial, arma_draws, log_target, nullptr, settings);
     std::cout << "MCMC Done - Chain Number " << omp_get_thread_num() << std::endl;
 
-    Eigen::MatrixXd draws = Eigen::Map<Eigen::MatrixXd>(arma_draws.memptr(), arma_draws.n_rows, arma_draws.n_cols);
-    Eigen::VectorXd c = (draws.col(0)*RW_SCALING).array().exp();
-    Eigen::Matrix3Xd a = draws.rightCols(3).transpose()*RW_SCALING;
-    softmax(a);
+    // Convert to Eigen and display results
+    auto draws = Eigen::Map<Eigen::MatrixXd>(arma_draws.memptr(), arma_draws.n_rows, arma_draws.n_cols);
+    draws *= RW_SCALING;
+    auto c = (draws.col(0)).array().exp();
+    auto a = draws.rightCols(3).transpose();
+    apply_softmax(a);
     std::cout << "c: " << c.mean() << std::endl;
     std::cout << "a: " << a.rowwise().mean() << std::endl;
     std::cout << "Accept: " << settings.rwmh_accept_rate << std::endl;
